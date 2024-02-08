0e9af9da962b6558be78f8ebb745a149af976613
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-02-09 04:41:27 +0530

    checkpoint-3

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,53 @@ void main() {
           backgroundColor: Colors.white,
           elevation: 0,
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(20),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 'Find Your',
                 style: TextStyle(color: Colors.black87, fontSize: 25),
               ),
-              SizedBox(height: 5),
-              Text(
+              const SizedBox(height: 5),
+              const Text(
                 'Inspiration',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
                 ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(244, 243, 243, 1),
+                    borderRadius: BorderRadius.circular(15)),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black87,
+                    ),
+                    border: InputBorder.none,
+                    hintText: "Search you're looking for",
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'Promo Today',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
             ],
           ),
