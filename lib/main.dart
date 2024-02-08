3644b67b1a27f58442a3c1df75843a329ccd6b5d
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-02-09 04:18:25 +0530

    checkpoint-2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
     MaterialApp(
       title: "Flutter Workshop App1",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Roboto'),
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -14,6 +15,27 @@ void main() {
           ),
           backgroundColor: Colors.white,
           elevation: 0,
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Find Your',
+                style: TextStyle(color: Colors.black87, fontSize: 25),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'Inspiration',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     ),
